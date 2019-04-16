f74585f86a489df4a769cc2b0022c25eb81df02f
zlongbin <2569050087@qq.com>
2019-04-16 10:33:25 +0000

    info

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
 });
 
 
+Route::get('/info', function () {
+   phpinfo();
+});
 
 Route::get('weixin/valid',"Weixin\WeixinController@valid");
 Route::post('weixin/valid',"Weixin\WeixinController@wxEvent");
