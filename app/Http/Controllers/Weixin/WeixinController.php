80e469b69be71d23c57e263e00df52ae31f8ef69
zlongbin <2569050087@qq.com>
2019-04-16 06:44:24 +0000

    天气

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -24,17 +24,18 @@ class WeixinController extends Controller
         $str =$time.$content."\n";
         // 日志
         file_put_contents("logs/weixin_event.log",$str,FILE_APPEND);
-        $data = simplexml_load_string($content);
+        $xml_obj = simplexml_load_string($content);
         // echo 'ToUserName: '. $data->ToUserName;echo '</br>';        // 公众号ID
         // echo 'FromUserName: '. $data->FromUserName;echo '</br>';    // 用户OpenID
         // echo 'CreateTime: '. $data->CreateTime;echo '</br>';        // 时间戳
         // echo 'MsgType: '. $data->MsgType;echo '</br>';              // 消息类型
         // echo 'Event: '. $data->Event;echo '</br>';                  // 事件类型
         // echo 'EventKey: '. $data->EventKey;echo '</br>';
-        $wx_id = $data->ToUserName;             // 公众号ID
-        $openid = $data->FromUserName;          //用户OpenID
-        $event = $data->Event;          //事件类型
-        if($event='subscribe'){
+        $wx_id = $xml_obj->ToUserName;             // 公众号ID
+        $openid = $xml_obj->FromUserName;          //用户OpenID
+        $event = $xml_obj->Event;                  //事件类型
+        $msg_type = $xml_obj->MsgType;             // 消息类型
+        if($event=='subscribe'){
             // 根据openid判断用户是否存在
             $local_user = WxUserModel::where(['openid'=>$openid])->first();
             if($local_user){
@@ -42,6 +43,7 @@ class WeixinController extends Controller
             }else{
                 // 获取用户信息
                 $user =$this->getUserInfo($openid);
+                // print_r($user) ;die;
                 // 用户信息入库
                 $user_Info=[
                     'openid'=>$user['openid'],
@@ -59,6 +61,38 @@ class WeixinController extends Controller
                 </Content>
                 </xml>';
             }
+        }elseif($msg_type=='text'){
+            if(strpos($xml_obj->Content,'+天气')){
+                $city=explode('+',$xml_obj->Content)[0];
+                echo "City : ".$city;
+                $url = "https://free-api.heweather.net/s6/weather/now?key=HE1904161042411866&location=".$city;
+                $arr = json_decode(file_get_contents($url),true);
+                // echo '<pre>';print_r($arr);echo "</pre>";               
+                $fl = $arr['HeWeather6'][0]['now']['fl'];               //摄氏度
+                $wind_dir = $arr['HeWeather6'][0]['now']['wind_dir'];   //风向
+                $wind_sc = $arr['HeWeather6'][0]['now']['wind_sc'];     //风力
+                $hum = $arr['HeWeather6'][0]['now']['hum'];             //湿度
+                $str="城市 : $city \n"."摄氏度 : $fl \n"."风向 : $wind_dir \n"."风力 : $wind_sc \n"."湿度 : $hum \n";
+
+                $response_xml='<xml>
+                <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                <FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+                <CreateTime>'.time().'</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA['.$str.']]>
+                </Content>
+                </xml>';
+            }else{
+                $response_xml='<xml>
+                <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                <FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+                <CreateTime>'.time().'</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[城市名不正确]]>
+                </Content>
+                </xml>';
+            }
+            return $response_xml;
         }
     }
     // 获取微信AccessToken
@@ -69,7 +103,6 @@ class WeixinController extends Controller
         if($token){
 
         }else{
-            // $url ="";
             $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET');
             $response=file_get_contents($url);
             $arr=json_decode($response,true);
@@ -87,6 +120,10 @@ class WeixinController extends Controller
         $data = file_get_contents($url);
         $user = json_decode($data,true);
         return $user;
+    }
+    public function s(){
+        // https://free-api.heweather.net/s6/weather/now?location=北京&key=HE1904161042411866
+    
     }
 }
 
