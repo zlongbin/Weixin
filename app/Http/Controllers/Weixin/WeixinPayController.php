156ba0d4e3f92f84220dcdfb42211d06ed572501
zlongbin <2569050087@qq.com>
2019-04-17 11:14:36 +0000

    notify

--- app/Http/Controllers/Weixin/WeixinPayController.php
+++ app/Http/Controllers/Weixin/WeixinPayController.php
@@ -153,7 +153,7 @@ class WeixinPayController extends Controller
         $data = file_get_contents("php://input");
         //记录日志
         $log_str = date('Y-m-d H:i:s') . "\n" . $data . "\n<<<<<<<";
-        file_put_contents('logs/wx_pay_notice.log',$log_str,FILE_APPEND);
+        file_put_contents('logs/wx_pay_notify.log',$log_str,FILE_APPEND);
         $xml = simplexml_load_string($data);
         if($xml->result_code=='SUCCESS' && $xml->return_code=='SUCCESS'){      //微信支付成功回调
             //验证签名
